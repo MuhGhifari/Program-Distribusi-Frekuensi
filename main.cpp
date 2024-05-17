768f825e8f2f1444fb9f3115f32850a434279ee7
MuhGhifari <muhammadghifari37@gmail.com>
2024-05-17 18:26:27 +0700

    v.1.0

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@
 #include <iomanip>
 #include <string>
 #include <algorithm>
+#include <sstream>
 
 using namespace std;
 
+string formatDesimal(const float &angka, const int &digit);
 void printTabel(const string judulTabel, const vector<vector<string>>& tabel, ofstream& output);
 
 int main() {
@@ -17,31 +19,35 @@ int main() {
   ifstream fileInput("input.txt");
 
   if (!fileInput) {
-    cerr << "[\x1b[31mERROR\x1b[0m] : file 'input.csv' tidak terdeteksi!" << endl;
+    cerr << "[\x1b[31mERROR\x1b[0m] : file 'input.txt' tidak terdeteksi!" << endl;
 		exit(1);
   }
 
+  int noBaris = 1;
   while (getline(fileInput, str)) {
     double nilaiInput;
     try {
       nilaiInput = stod(str);
     }
     catch (const invalid_argument& e) {
-      cerr << "Error: " << e.what() << endl;
+      cerr << "[\x1b[31mERROR\x1b[0m] : nilai '" << str << "' pada baris ke-" << noBaris << " tidak valid!"  << endl;
+      cerr << "[\x1b[33mWARNING\x1b[0m] : baris ke-" << noBaris << " dilewati..." << endl;
+      noBaris++;
       continue;
     }
 
     input.push_back(nilaiInput);
+    noBaris++;
   }
   fileInput.close();
 
-  double rata2, varians, standarDeviasi, standarError;
-  double n = input.size();
+  int n = input.size();
+  double sum = 0, rata2, varians, standarDeviasi, standarError;
 
   for (size_t i = 0; i < n; i++) {
-    rata2 += input[i];
+    sum += input[i];
   }
-  rata2 = rata2 / n;
+  rata2 = sum / n;
 
   for (size_t i = 0; i < n; i++) {
     varians += pow((input[i] - rata2), 2);
@@ -56,45 +62,48 @@ int main() {
   standarDeviasi = sqrt(varians);
   standarError = standarDeviasi / sqrt(n);
 
-  tabelDeskriptif = {
-    {"Rata-Rata", to_string(rata2)},
-    {"Varians", to_string(varians)},
-    {"Simpangan Baku", to_string(standarDeviasi)},
-    {"Standar Error", to_string(standarError)}
-  };
-
   sort(input.begin(), input.end());
-  auto max = max_element(input.begin(), input.end());
-  auto min = min_element(input.begin(), input.end());
-  double jangkauan = *max - *min;
-  double kelas = ceil(1 + (3.3 * log10(n)));
+  int max = *max_element(input.begin(), input.end());
+  int min = *min_element(input.begin(), input.end());
+  int jangkauan = max - min;
+  int kelas = ceil(1 + (3.3 * log10(n)));
   int interval = ceil(jangkauan / kelas);
-  
-  cout << "n\t\t: " << n << endl;
-  cout << "max\t\t: " << *max << endl;
-  cout << "min\t\t: " << *min << endl;
-  cout << "jangkauan\t: " << jangkauan << endl;
-  cout << "kelas\t\t: " << kelas << endl;
-  cout << "interval\t: " << interval << endl;
+
+  tabelDeskriptif = {
+    {"Jumlah Data", to_string(n)},
+    {"Nilai Maks", to_string(max)},
+    {"Nilai Min", to_string(min)},
+    {"Rata-Rata", formatDesimal(rata2, 1)},
+    {"Jangkauan", to_string(jangkauan)},
+    {"Kelas", to_string(kelas)},
+    {"Interval", to_string(interval)},
+    {"Varians", formatDesimal(varians, 3)},
+    {"Standar Deviasi", formatDesimal(standarDeviasi, 3)},
+    {"Standar Error", formatDesimal(standarError, 3)}
+  };
 
   vector<int> frekuensi(kelas, 0);
   vector<string> ketFrekuensi;
 
-  int awalan = *min;
+  int awalan = min, akhiran = awalan;
   int indeksInput = 0;
   for (size_t i = 0; i < kelas; i++) {
-    int akhiran = awalan + interval;
+    akhiran += interval;
     for (size_t j = indeksInput; j < input.size(); j++) {
+      if (i == kelas - 1 && input[j] == akhiran) {akhiran++;};
       if (input[j] < akhiran) {
         frekuensi[i]++;
         indeksInput++;
+      } else {
+        break;
       }
     }
-    str  = to_string(awalan) + "-" + to_string(akhiran - 1);
+    str  = to_string(awalan) + " - " + to_string(akhiran - 1);
     ketFrekuensi.push_back(str);
     awalan = akhiran;
   }
-  tabelDistribusi.push_back({"Jangkauan", "Frekuensi"});
+
+  tabelDistribusi.push_back({"Interval", "Frekuensi"});
   for (size_t i = 0; i < kelas; i++) {
     vector<string> baris;
     baris.push_back(ketFrekuensi[i]);
@@ -102,14 +111,21 @@ int main() {
     tabelDistribusi.push_back(baris);
   }
 
-  ofstream outputDeskriptif, outputDistribusi;
-  outputDeskriptif.open("Tabel Deskriptif.txt");
-  outputDistribusi.open("Tabel Distribusi Frekuensi.txt");
+  ofstream fileOutput;
+  fileOutput.open("output.txt");
 
-  printTabel("Tabel Deskriptif", tabelDeskriptif, outputDeskriptif);
-  printTabel("Tabel Distribusi Frekuensi", tabelDistribusi, outputDistribusi);
+  printTabel("Tabel Distribusi Frekuensi", tabelDistribusi, fileOutput);
+  fileOutput << endl;
+  printTabel("Keterangan", tabelDeskriptif, fileOutput);
+  fileOutput.close();
   
   return 0;
+}
+
+string formatDesimal(const float &angka, const int &digit) {
+  stringstream ss;
+  ss << fixed << setprecision(digit) << angka;
+  return ss.str();
 }
 
 void printTabel(const string judulTabel, const vector<vector<string>>& tabel, ofstream& output) {
@@ -154,5 +170,4 @@ void printTabel(const string judulTabel, const vector<vector<string>>& tabel, of
     output << "+"<< string(lebarKolom[i] + 2, '-');
   }
   output << "+";
-  output.close();
 }
